cf813f0810ae85f9716fadb5a7d68719abb8bc5f
kamil5b <zee312kamilerz@gmail.com>
2023-11-01 05:11:43 +0700

    driver update

--- driver.go
+++ driver.go
@@ -16,6 +16,14 @@ func SetupTemplate(server_url, db_url, user, password, protocol, db string) {
 	database.Connect(
 		db_url, user, password, protocol, db,
 		&models.User{},
+		&models.Account{},
+		&models.Store{},
+		&models.Item{},
+		&models.InvoiceHistory{},
+		&models.Inventory{},
+		&models.CreditInvoice{},
+		&models.DebitInvoice{},
+		&models.Transaction{},
 	)
 	app := fiber.New()
 	/*
