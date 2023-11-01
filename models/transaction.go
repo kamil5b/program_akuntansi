f6aef00283d837923004a1eab1f0db6b437ffd70
kamil5b <zee312kamilerz@gmail.com>
2023-11-01 14:05:19 +0700

    delete invoice field in transaction

--- models/transaction.go
+++ models/transaction.go
@@ -10,7 +10,6 @@ type Transaction struct {
 	Inventory   Inventory `json:"inventory"`
 	InvoiceID   uint      `json:"invoice_id"`
 	InvoiceType string    `json:"invoice_type"` //Credit/Debit (Out/In)
-	Invoice     Invoice   `json:"invoice"`
 	TotalPrice  uint      `json:"total_price"`
 	Discount    uint      `json:"discount"`
 }
