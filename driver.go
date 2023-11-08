6b0b79c757e70f2bce4bbddd8c520d90da2f0585
kamil5b <zee312kamilerz@gmail.com>
2023-11-09 05:10:54 +0700

    simplify

--- driver.go
+++ driver.go
@@ -8,14 +8,16 @@ import (
 
 func main() {
 	var (
-		acc_url  = utilities.GoDotEnvVariable("ACCOUNTANCY_URL")
-		auth_url = utilities.GoDotEnvVariable("AUTH_URL")
-		db_url   = utilities.GoDotEnvVariable("DATABASE_URL")
-		user     = utilities.GoDotEnvVariable("DATABASE_USER")
-		password = utilities.GoDotEnvVariable("DATABASE_PASSWORD")
-		protocol = utilities.GoDotEnvVariable("DSN_PROTOCOL")
-		db       = utilities.GoDotEnvVariable("DATABASE_NAME")
+
+		//server_url  = utilities.GoDotEnvVariable("SERVER_PORT")
+		acc_port  = utilities.GoDotEnvVariable("ACCOUNTANCY_PORT")
+		auth_port = utilities.GoDotEnvVariable("AUTH_PORT")
+		db_url    = utilities.GoDotEnvVariable("DATABASE_URL")
+		user      = utilities.GoDotEnvVariable("DATABASE_USER")
+		password  = utilities.GoDotEnvVariable("DATABASE_PASSWORD")
+		protocol  = utilities.GoDotEnvVariable("DSN_PROTOCOL")
+		db        = utilities.GoDotEnvVariable("DATABASE_NAME")
 	)
-	go auth_service.SetupTemplate(auth_url, db_url, user, password, protocol, db)
-	accountancy_service.SetupTemplate(acc_url, db_url, user, password, protocol, db)
+	go auth_service.SetupTemplate(auth_port, db_url, user, password, protocol, db)
+	accountancy_service.SetupTemplate(acc_port, db_url, user, password, protocol, db)
 }
